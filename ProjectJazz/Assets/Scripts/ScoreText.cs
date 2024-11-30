13894cf4d6e49c23bc386837828dd0dabbd5bb03
ArcherStudio <lutielong724@gmail.com>
2024-11-30 19:17:47 +0800

    Import assets, finish score animation

---
+++ ProjectJazz/Assets/Scripts/ScoreText.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ScoreText : MonoBehaviour
+{
+    public float scaleTime;
+    public float scaleMultiplier;
+
+    private int _previousScore;
+    private int _currentScore;
+
+    private void ScaleScore()
+    {
+        transform.DOScale(transform.localScale * scaleMultiplier, scaleTime);
+        transform.DOScale(transform.localScale, scaleTime).SetDelay(scaleTime);
+    }
+
+    private void Update()
+    {
+        _currentScore = GameManager.instance.currentScore;
+        if (_currentScore != _previousScore)
+        {
+            ScaleScore();
+            _previousScore = _currentScore;
+        }
+    }
+}
